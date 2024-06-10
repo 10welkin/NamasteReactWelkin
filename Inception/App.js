06f008f9f08a7b7a6334cb7d83606dea5013d8d1
Gaurav Phokmare <gkphokmare@gmail.com>
2024-06-10 12:22:08 +0530

    new epdisode 3

--- Inception/App.js
+++ Inception/App.js
@@ -1,8 +1,3 @@
 import React from "react";
 import ReactDOM from "react-dom/client"
 
-const heading = React.createElement("h1", {id: "heading"}, "Hello wolrd");
-
-const root = ReactDOM.createRoot(document.getElementById("root"));
-
-root.render(heading)
