dc321c193e47b7333fa056e6109dbe5e2a3a68b8
Gaurav Phokmare <gkphokmare@gmail.com>
2024-06-11 10:11:51 +0530

    epsiode 3

--- Inception/App.js
+++ Inception/App.js
@@ -1,3 +1,22 @@
 import React from "react";
 import ReactDOM from "react-dom/client"
 
+const Title = () => (
+    <h1>head</h1>
+)
+
+const title = (
+    <h1>head</h1>
+)
+
+const HeadingComponent = () => (
+    <>
+            {title}
+            <h1>HEllo</h1>
+            <Title />
+    </>
+    )
+
+const root = ReactDOM.createRoot(document.getElementById("root"))
+
+root.render(<HeadingComponent />)
